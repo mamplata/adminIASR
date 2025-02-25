8daa40040e7191f72cd297d36d7fa20aa0091213
mamplata <deejaymamplata29@gmail.com>
2025-02-25 16:43:24 +0800

    .

--- admin/resources/js/Components/DaisyTable.vue
+++ admin/resources/js/Components/DaisyTable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="overflow-x-auto">
-        <table v-if="data.length > 0" class="table w-full bg-white dark:bg-gray-900 shadow-md rounded-lg">
+        <table v-if="data.length > 0" class="table table-fixed w-full bg-white dark:bg-gray-900 shadow-md rounded-lg">
             <!-- Table Head -->
             <thead>
                 <tr class="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-b">
