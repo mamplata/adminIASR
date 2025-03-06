d888d21e68caee1c5b00b6c66b41a93467e4bdf2
mamplata <deejaymamplata29@gmail.com>
2025-03-06 08:39:37 +0800

    add front end

---
+++ vue-frontend/src/components/RecentLog.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="table-responsive">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Photo</th>
+                    <th>Name</th>
+                    <th>Program</th>
+                    <th>Department</th>
+                    <th>Year Level</th>
+                    <th>Timestamp</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>hehe</td>
+                    <td>Kyle Malaki etits</td>
+                    <td>IT</td>
+                    <td>CCS</td>
+                    <td>4th Year</td>
+                    <td>00:04:03-00:04:07</td>
+                </tr>
+                <tr>
+                    <td>hehe</td>
+                    <td>Boncales Malaki etits</td>
+                    <td>IT</td>
+                    <td>CCS</td>
+                    <td>4th Year</td>
+                    <td>00:04:04-00:04:07</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.table-responsive {
+    width: 90%;
+    overflow-x: auto;
+}
+
+.table {
+    width: 100% !important;
+    table-layout: auto;
+}
+</style>
