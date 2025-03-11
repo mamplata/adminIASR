7771d994ecdde485171d7a5deca77e8e7eca4791
mamplata <deejaymamplata29@gmail.com>
2025-03-11 15:58:55 +0800

    setup up daisy ui for vuefrontend2 testing

--- vue-frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-createApp(App).mount("#app");
-
-import "bootstrap/dist/js/bootstrap.min.js";
--- vue-frontend2/vite.config.js
+++ vue-frontend2/vite.config.js
@@ -1,13 +1,11 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue()], // Remove TailwindCSS from here
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Ensure '@' points to the 'src' directory
+      "@": path.resolve(__dirname, "src"), // Ensure '@' points to 'src'
     },
   },
 });
