9499d2fb3aa2eb6e73a811ac58ab3d99eed61126
aconite12 <ravenglenn312@gmail.com>
2025-03-12 13:19:00 +0800

    .

--- admin/database/seeders/DatabaseSeeder.php
+++ admin/database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call([UserSeeder::class]);
+        $this->call([UserSeeder::class]);
         $this->call([SemesterSeeder::class]);
-        // $this->call(DepartmentProgramSeeder::class);
+        $this->call(DepartmentProgramSeeder::class);
     }
 }
