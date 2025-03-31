53f3b6f35ec32380cb007c4fc14388d10b2b214a
aconite12 <ravenglenn312@gmail.com>
2025-03-31 13:26:19 +0800

    [S2] Added background image and logo centered in the card.

--- vue-frontend/src/components/DeviceRegistration.vue
+++ vue-frontend/src/components/DeviceRegistration.vue
@@ -1,17 +1,29 @@
 <template>
-    <div class="flex items-center justify-center h-screen">
-        <div class="card custom-card bg-base-200 shadow-xl">
-            <div class="card-body custom-card-body">
-                <h2 class="card-title custom-card-title mb-[2vh]">Register Device</h2>
-                <form @submit.prevent="registerDevice" class="space-y-[2vh]">
-                    <input v-model="shortCode" type="text" placeholder="Enter short code"
-                        class="input input-bordered w-full custom-input" required />
-                    <button type="submit"
-                        class="w-full btn text-white btn-success shadow-lg hover:bg-[#20714c] custom-button">
-                        Register
-                    </button>
-                </form>
-                <p v-if="errorMessage" class="text-red-500 mt-[2vh]">{{ errorMessage }}</p>
+    <div class="relative h-screen w-screen">
+        <!-- Background Image -->
+        <img :src="pncBg" alt="Background" class="absolute top-0 left-0 w-full h-full object-cover" />
+
+        <!-- Content Container -->
+        <div class="flex items-center justify-center h-screen relative z-10">
+            <div class="relative flex flex-col items-center">
+
+                <!-- Card -->
+                <div class="card custom-card bg-base-200 shadow-xl relative z-10">
+                    <img :src="pncLogo" alt="PNC Logo" class="w-[10vw] mx-auto h-auto mb-[-4vh] z-20" />
+
+                    <div class="card-body custom-card-body">
+                        <h2 class="card-title custom-card-title mb-[2vh] text-center">Register Device</h2>
+                        <form @submit.prevent="registerDevice" class="space-y-[2vh]">
+                            <input v-model="shortCode" type="text" placeholder="Enter short code"
+                                class="input input-bordered w-full custom-input" required />
+                            <button type="submit"
+                                class="w-full btn text-white btn-success shadow-lg hover:bg-[#20714c] custom-button">
+                                Register
+                            </button>
+                        </form>
+                        <p v-if="errorMessage" class="text-red-500 mt-[2vh] text-center">{{ errorMessage }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -20,6 +32,8 @@
 <script setup>
 import { ref } from 'vue';
 import { useDeviceStore } from '@/stores/deviceStore';
+import pncBg from '../assets/img/bgAnnounce.png';
+import pncLogo from '../assets/img/ITAPLOGO.png';
 
 const shortCode = ref('');
 const errorMessage = ref('');
